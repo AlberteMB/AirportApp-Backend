c84fb364e64e107c4be372dc0d41d2263f984f55
AlberteMB <albertebardanca@hotmail.com>
2025-03-21 08:45:40 +0100

    First commit

---
+++ src/main/java/amb/AirportAppBackend/AirportAppBackendApplication.java
@@ -0,0 +1,13 @@
+package amb.AirportAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/amb/AirportAppBackend/AirportAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package amb.AirportAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
